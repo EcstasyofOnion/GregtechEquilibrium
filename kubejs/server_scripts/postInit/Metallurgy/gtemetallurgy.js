30a04a0c3ca76520989fa6cab3659c2f72ea9a58
Reisaka <140258572+EcstasyofOnion@users.noreply.github.com>
2025-02-13 21:38:08 -0500

    Update gtemetallurgy.js

--- kubejs/server_scripts/postInit/Metallurgy/gtemetallurgy.js
+++ kubejs/server_scripts/postInit/Metallurgy/gtemetallurgy.js
@@ -173,134 +173,134 @@ ServerEvents.recipes(event => {
 
     event.recipes.gtceu.primitive_blast_furnace('b1')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '4x gtceu:high_purity_carbon_dust'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '4x gtceu:tiny_ash_dust'
         )
-        .duration(480)
+        .duration(320)
 
     event.recipes.gtceu.primitive_blast_furnace('b2')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '4x gtceu:carbon_dust'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '4x gtceu:tiny_ash_dust'
         )
-        .duration(480)
+        .duration(320)
 
     event.recipes.gtceu.primitive_blast_furnace('b3')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '4x gtceu:coke'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '4x gtceu:tiny_ash_dust'
         )
-        .duration(960)
+        .duration(640)
 
     event.recipes.gtceu.primitive_blast_furnace('b4')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '4x gtceu:coke_dust'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '4x gtceu:tiny_ash_dust'
         )
-        .duration(960)
+        .duration(640)
 
     event.recipes.gtceu.primitive_blast_furnace('b5')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '4x gtceu:anthracite'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '4x gtceu:tiny_ash_dust'
         )
-        .duration(960)
+        .duration(640)
 
     event.recipes.gtceu.primitive_blast_furnace('b6')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '4x gtceu:anthracite_dust'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '4x gtceu:tiny_ash_dust'
         )
-        .duration(960)
+        .duration(640)
 
     event.recipes.gtceu.primitive_blast_furnace('b7')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '8x gtceu:lignite_coke'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '8x gtceu:tiny_ash_dust'
         )
-        .duration(1440)
+        .duration(960)
 
     event.recipes.gtceu.primitive_blast_furnace('b8')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '8x gtceu:lignite_coke_dust'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '8x gtceu:tiny_ash_dust'
         )
-        .duration(1440)
+        .duration(960)
 
     event.recipes.gtceu.primitive_blast_furnace('b9')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '8x minecraft:coal'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '8x gtceu:tiny_dark_ash_dust'
         )
-        .duration(1920)
+        .duration(1280)
 
     event.recipes.gtceu.primitive_blast_furnace('b10')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '8x gtceu:coal_dust'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '8x gtceu:tiny_dark_ash_dust'
         )
-        .duration(1920)
+        .duration(1280)
 
     event.recipes.gtceu.primitive_blast_furnace('b11')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '8x minecraft:charcoal'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '8x gtceu:tiny_dark_ash_dust'
         )
-        .duration(1920)
+        .duration(1280)
 
     event.recipes.gtceu.primitive_blast_furnace('b12')
         .itemInputs(
-            '2x gtceu:magnetite_dust',
+            '2x gtceu:banded_iron_dust',
             '8x gtceu:charcoal_dust'
         )
         .itemOutputs(
             '6x gtceu:pig_iron_ingot',
             '8x gtceu:tiny_dark_ash_dust'
         )
-        .duration(1920)
+        .duration(1280)
 
 })
