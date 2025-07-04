54d40e39fb5d7c8a93d20e80e9cdcfdc4a9b7781
Reisaka <140258572+EcstasyofOnion@users.noreply.github.com>
2025-07-04 16:45:24 -0400

    changes

---
+++ kubejs/server_scripts/ore_type_tags.js
@@ -0,0 +1,4 @@
+ServerEvents.tags('block', event => {
+    event.add('minecraft:stone_ore_replaceables', 'strata:komatiite')
+    event.add('minecraft:stone_ore_replaceables', 'what_is_stone:black_marble')
+})
--- kubejs/startup_scripts/removeoregen.js
+++ kubejs/startup_scripts/removeoregen.js
@@ -1,9 +1,5 @@
 WorldgenEvents.remove(event => {
   event.removeOres(props => {
-    // much like ADDING ores, this supports filtering by worldgen layer...
-    props.worldgenLayer = 'underground_ores'
-    // BlockStatePredicate to remove iron and copper ores from the given biomes
-    // Note tags may NOT be used here, unfortunately...
     props.blocks = [
       'railcraft:zinc_ore', 'railcraft:deepslate_zinc_ore', 
       'railcraft:silver_ore', 'railcraft:deepslate_silver_ore', 
@@ -14,7 +10,6 @@ WorldgenEvents.remove(event => {
       'railcraft:saltpeter_ore',
       'blocksyouneed_luna:aquatrine_sand_ore', 'blocksyouneed_luna:aquatrine_sandstone_ore',
       'blocksyouneed_luna:sodalite',
-      'blocksyouneed_luna:shale',
       'blocksyouneed_luna:achoria',
       'blocksyouneed_luna:slime_achoria',
       'blocksyouneed_luna:moonstone_uc',
@@ -25,5 +20,8 @@ WorldgenEvents.remove(event => {
       'ytech:nether_copper_ore',
       'ytech:galena_ore', 'ytech:deepslate_galena_ore', 'ytech:nether_galena_ore',
       'ytech:cassiterite_ore', 'ytech:deepslate_cassiterite_ore', 'ytech:nether_cassiterite_ore']
+  })
+  event.removeOres(ores => {
+    ores.blocks = ['blocksyouneed_luna:shale']
   })
 })
